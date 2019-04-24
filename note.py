020aec3084898b227fe43e7cf261ec73696adeb5
Lee <AnOutburst@mail.com>
2019-04-24 13:54:47 -0500

    visual crud

--- note.py
+++ note.py
@@ -10,29 +10,42 @@ class pad:
     def __init__(self):
         print("hello")
         self.l = tkinter.StringVar()
-        self.refresh_notes()
         gui = tkinter.Tk()
         gui.configure(background="#008080")
         gui.title("Note Taking")
         gui.geometry("300x600")
-        addButton = tkinter.Button(gui, text="add a note", command=self.add_note, bg="#008080")
-        self.e = tkinter.Entry(gui)
-        self.e.insert(0, "number to delete")
-        deleteButton = tkinter.Button(gui, text="delete entered item", command=self.delete_note, bg="#008080")
+        addButton = tkinter.Button(gui, text="add a note", command=self.add_note, bg="#008080", width=40)
+        self.e = tkinter.Entry(gui, width=50)
+        self.e.insert(0, "note text here")
+        deleteButton = tkinter.Button(gui, text="delete selected item", command=self.delete_note, bg="#008080", width=40)
+
         #itemlist = tkinter.Label(gui, textVariable=self.l)
+        self.scrollbar = tkinter.Scrollbar(gui, orient=tkinter.VERTICAL)
+        self.itemlist = tkinter.Listbox(gui, selectmode=tkinter.SINGLE, width=50, height=6,
+                                        yscrollcommand=self.scrollbar.set)
+        self.scrollbar.config(command=self.itemlist.yview)
+        #self.itemlist.pack()
+        self.itemlist['yscroll'] = self.scrollbar.set
+
         addButton.grid(column=0,row=0)
         self.e.grid(column=0,row=1)
         deleteButton.grid(column=0,row=2)
+        self.itemlist.grid(column=0, row=4, columnspan=2)
+        #self.scrollbar.grid()
+
+        self.refresh_notes()
 
         gui.mainloop()
 
     def refresh_notes(self):
+        self.itemlist.delete(0,tkinter.END)
         print("refreshing")
         self.notes = []
         stringhold = ""
         index = 1
         for row in self.c.execute('SELECT * FROM notes '):
             held = ''.join(row)
+            self.itemlist.insert(tkinter.END, held)
             stringhold +=  str(index) + ". " + held + "\n"
             index+=1
             self.notes.append(held)
@@ -48,7 +61,8 @@ class pad:
             print("note " + str(index) + ": " + note)
 
     def add_note(self):
-        action = input("what would you like to say")
+        #action = input("what would you like to say")
+        action = str(self.e.get())
         self.c.execute("INSERT INTO notes VALUES (?)", (action,))
         self.conn.commit()
         self.refresh_notes()
@@ -57,9 +71,9 @@ class pad:
         print("note " + action + " added as number " + str(len(self.notes)))
 
     def delete_note(self):
-        num = 0
         try:
-            num = int(self.e.get())
+            num = self.itemlist.curselection()[0] + 1
+            #num = int(self.e.get())
             if 0 < num <= len(self.notes):
                 print("cool")
                 valu = self.notes[num-1]
@@ -71,33 +85,3 @@ class pad:
                 print("bad")
         except:
             print("you fucked up bozo")
-
-
-
-
-
-# if len(self.notes)<=1 :
-#     print("you cannot delete all your notes fool!")
-# else:
-#     self.say_notes()
-#     action = input("what note would you like to delete")
-#     try:
-#         del self.notes[int(action)-1]
-#     except:
-#         print("delete value must be a number between 1 and " + str(len(self.notes)))
-
-# if __name__ == 'note':
-#     myNote = pad()
-#     print("Welcome to notes!")
-#     myNote.say_notes()
-#     while True:
-#         action = input("add note [A], delete note [D], show notes [S]").upper()
-#         if action not in "ADS" or len(action) != 1:
-#             print("invalid val")
-#             continue
-#         if action == "A":
-#             myNote.add_note()
-#         elif action =="D":
-#             myNote.delete_note()
-#         elif action =="S":
-#             myNote.say_notes()
