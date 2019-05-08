34f59cf463e9ed2388814f4a14e5eaa71bd00d64
Lee <AnOutburst@mail.com>
2019-05-08 16:57:56 -0500

    add clock

--- run.py
+++ run.py
@@ -4,6 +4,9 @@ import calculator
 import note
 import klondikesolitaire
 import media
+import time
+
+time1 = ''
 
 #all code unless otherwise stated written by Lee Hayes for his capstone project
 #Klondike Solitaire is an opensource project created by artexcercise on github and can be found here
@@ -21,6 +24,18 @@ def runsolitaire():
 def runMedia():
     mp3 = media.mp3()
 
+def tick():
+    global time1
+    # get the current local time from the PC
+    time2 = time.strftime('%H:%M:%S')
+    # if time string has changed, update it
+    if time2 != time1:
+        time1 = time2
+        clock.config(text=time2)
+    # calls itself every 200 milliseconds
+    # to update the time display as needed
+    # could use >200 ms, but display gets jerky
+    clock.after(200, tick)
 
 if __name__ == '__main__':
     conn = sqlite3.connect('note.db')
@@ -40,6 +55,8 @@ if __name__ == '__main__':
     noteButton = tkinter.Button(window, image=noteImg, command=runNote, bg="#008080")
     solButton = tkinter.Button(window, image=solImg, command=runsolitaire, bg="#008080")
     mediaButton = tkinter.Button(window, image=mediaImg, command=runMedia, bg="#008080")
+
+
     #label.grid(column=0, row=0)
     #calcButton.grid(column=0, row=1, ipadx=10, ipady=10)
     #noteButton.grid(column=0, row=2, ipadx=10, ipady=10)
@@ -49,5 +66,12 @@ if __name__ == '__main__':
     solButton.place(width=50, height=50, x=20, y=160)
     mediaButton.place(width=50, height=50, x=20, y=230)
 
+    bottom = tkinter.Canvas(window, width=1280, height=35, bg="#B9BABD")
+    #bottom.create_rectangle(0,0,1280,35, fill="#B9BABD")
+    clock = tkinter.Label(bottom, font=('times', 20, 'bold'), bg='#dedee0', text="test")
+    clock.place(x=1210)
+    bottom.place(x=0, y=685)
+
+    tick()
 
     window.mainloop()
